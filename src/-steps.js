568060ba2c6ecaaddd0605d4cfce17013f1f6a29
Kamrul Hasan <kamrulsdev@gmail.com>
2022-04-05 23:20:06 +0600

    Recap google popup sign in process with 11 steps

--- src/-steps.js
+++ src/-steps.js
@@ -1,12 +1,14 @@
 /* 
  * Steps to use firebase
     1. Create a project on console.firebase.google.com
-    2. install firebase
+    2. npm install firebase
     3. Register web app in firebase
     4. copy firebase init with config from firebase project settings into a file firebase.init.js
-    5. export default app;
-    6. import app firebase.init.js into your app.js
-    7. import getAuth from firebase/auth and create auth = getAuth(app)
+    5. export default app; from firebase.init.js
+    6. import getAuth from firebase/auth and create const auth = getAuth(app) in app.js
+    7. import app firebase.init.js into your app.js
     8. turn on google authentication(firebase> authentication>enable google signin)
     9. create google provider
+    10. use signInWithPopup and pass aut and provider
+    11. handle .then (if successfull) and catch error (if error)
 */
